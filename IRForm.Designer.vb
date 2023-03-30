f575b4c629c6cb7cf29d0634240b161e97b8480b
CL Bogart <clbogart3@outlook.com>
2023-03-30 10:54:53 -0600

    Add project files.

---
+++ IRForm.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class IRForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.GimbalButton1 = New System.Windows.Forms.Button()
+        Me.GimablButton2 = New System.Windows.Forms.Button()
+        Me.GimbalButton3 = New System.Windows.Forms.Button()
+        Me.GimbalButton4 = New System.Windows.Forms.Button()
+        Me.HomeButton = New System.Windows.Forms.Button()
+        Me.VideoOnButton = New System.Windows.Forms.Button()
+        Me.VideoOffButton = New System.Windows.Forms.Button()
+        Me.FFCButton = New System.Windows.Forms.Button()
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'GimbalButton1
+        '
+        Me.GimbalButton1.Location = New System.Drawing.Point(181, 47)
+        Me.GimbalButton1.Name = "GimbalButton1"
+        Me.GimbalButton1.Size = New System.Drawing.Size(75, 23)
+        Me.GimbalButton1.TabIndex = 3
+        Me.GimbalButton1.Text = "0, -70"
+        Me.GimbalButton1.UseVisualStyleBackColor = True
+        '
+        'GimablButton2
+        '
+        Me.GimablButton2.Location = New System.Drawing.Point(181, 76)
+        Me.GimablButton2.Name = "GimablButton2"
+        Me.GimablButton2.Size = New System.Drawing.Size(75, 23)
+        Me.GimablButton2.TabIndex = 4
+        Me.GimablButton2.Text = "180, -70"
+        Me.GimablButton2.UseVisualStyleBackColor = True
+        '
+        'GimbalButton3
+        '
+        Me.GimbalButton3.Location = New System.Drawing.Point(181, 105)
+        Me.GimbalButton3.Name = "GimbalButton3"
+        Me.GimbalButton3.Size = New System.Drawing.Size(75, 23)
+        Me.GimbalButton3.TabIndex = 5
+        Me.GimbalButton3.Text = "90, -25"
+        Me.GimbalButton3.UseVisualStyleBackColor = True
+        '
+        'GimbalButton4
+        '
+        Me.GimbalButton4.Location = New System.Drawing.Point(181, 134)
+        Me.GimbalButton4.Name = "GimbalButton4"
+        Me.GimbalButton4.Size = New System.Drawing.Size(75, 23)
+        Me.GimbalButton4.TabIndex = 6
+        Me.GimbalButton4.Text = "270, -25"
+        Me.GimbalButton4.UseVisualStyleBackColor = True
+        '
+        'HomeButton
+        '
+        Me.HomeButton.Location = New System.Drawing.Point(181, 187)
+        Me.HomeButton.Name = "HomeButton"
+        Me.HomeButton.Size = New System.Drawing.Size(75, 23)
+        Me.HomeButton.TabIndex = 7
+        Me.HomeButton.Text = "Home"
+        Me.HomeButton.UseVisualStyleBackColor = True
+        '
+        'VideoOnButton
+        '
+        Me.VideoOnButton.Location = New System.Drawing.Point(100, 187)
+        Me.VideoOnButton.Name = "VideoOnButton"
+        Me.VideoOnButton.Size = New System.Drawing.Size(75, 23)
+        Me.VideoOnButton.TabIndex = 8
+        Me.VideoOnButton.Text = "Video On"
+        Me.VideoOnButton.UseVisualStyleBackColor = True
+        '
+        'VideoOffButton
+        '
+        Me.VideoOffButton.Location = New System.Drawing.Point(100, 216)
+        Me.VideoOffButton.Name = "VideoOffButton"
+        Me.VideoOffButton.Size = New System.Drawing.Size(75, 23)
+        Me.VideoOffButton.TabIndex = 9
+        Me.VideoOffButton.Text = "Video Off"
+        Me.VideoOffButton.UseVisualStyleBackColor = True
+        '
+        'FFCButton
+        '
+        Me.FFCButton.Location = New System.Drawing.Point(19, 216)
+        Me.FFCButton.Name = "FFCButton"
+        Me.FFCButton.Size = New System.Drawing.Size(75, 23)
+        Me.FFCButton.TabIndex = 10
+        Me.FFCButton.Text = "FFC/Adjust"
+        Me.FFCButton.UseVisualStyleBackColor = True
+        '
+        'ResetButton
+        '
+        Me.ResetButton.Location = New System.Drawing.Point(19, 187)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(75, 23)
+        Me.ResetButton.TabIndex = 11
+        Me.ResetButton.Text = "Reset Turret"
+        Me.ResetButton.UseVisualStyleBackColor = True
+        '
+        'IRForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(322, 255)
+        Me.Controls.Add(Me.ResetButton)
+        Me.Controls.Add(Me.FFCButton)
+        Me.Controls.Add(Me.VideoOffButton)
+        Me.Controls.Add(Me.VideoOnButton)
+        Me.Controls.Add(Me.HomeButton)
+        Me.Controls.Add(Me.GimbalButton4)
+        Me.Controls.Add(Me.GimbalButton3)
+        Me.Controls.Add(Me.GimablButton2)
+        Me.Controls.Add(Me.GimbalButton1)
+        Me.Name = "IRForm"
+        Me.Text = "IR Control Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents GimbalButton1 As Button
+    Friend WithEvents GimablButton2 As Button
+    Friend WithEvents GimbalButton3 As Button
+    Friend WithEvents GimbalButton4 As Button
+    Friend WithEvents HomeButton As Button
+    Friend WithEvents VideoOnButton As Button
+    Friend WithEvents VideoOffButton As Button
+    Friend WithEvents FFCButton As Button
+    Friend WithEvents ResetButton As Button
+End Class
---
+++ IRForm.vb
@@ -0,0 +1,145 @@
+﻿Public Class IRForm
+    Dim headerByte(17) As Byte
+
+    Private Sub IRForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        SerialPort1.PortName = "COM5" 'name serial port
+        SerialPort1.BaudRate = 57600  'set baud rate 19.2k
+        SerialPort1.DataBits = 8 'number of data bits is 8
+        SerialPort1.StopBits = IO.Ports.StopBits.One 'one stop bit
+        SerialPort1.Parity = IO.Ports.Parity.None 'no parity bits
+        SerialPort1.Open() 'intialize and open port
+        'Timer1.Enabled = True 'enable timer 1 on form
+
+        headerByte(0) = &H55
+        headerByte(1) = &HAA
+        headerByte(2) = &H7
+        headerByte(3) = &H4D
+        headerByte(4) = &H0
+        headerByte(5) = &H1
+        headerByte(6) = &H0
+        headerByte(7) = &H98
+        headerByte(8) = &H0
+        headerByte(9) = &H0
+        headerByte(10) = &H0
+        headerByte(11) = &H0
+        headerByte(12) = &H0
+        headerByte(13) = &H0
+        headerByte(14) = &H0
+        headerByte(15) = &H0
+        headerByte(16) = &H0
+        headerByte(17) = &H0
+
+        'For row As Integer = 0 To 8
+        '    ListBox1.Items.Add(Hex(headerByte(row)))
+        'Next
+
+    End Sub
+
+    Private Sub GimbalButton1_Click(sender As Object, e As EventArgs) Handles GimbalButton1.Click, HomeButton.Click
+        headerByte(9) = &H47
+        headerByte(10) = &H4A
+        headerByte(11) = &H19
+        headerByte(12) = &HB8
+        headerByte(13) = &H8B
+        headerByte(14) = &H0
+        headerByte(15) = &H80
+        headerByte(16) = &H31
+        headerByte(17) = &H53
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+
+    Private Sub GimablButton2_Click(sender As Object, e As EventArgs) Handles GimablButton2.Click
+        headerByte(9) = &H47
+        headerByte(10) = &H4A
+        headerByte(11) = &H19
+        headerByte(12) = &H48
+        headerByte(13) = &H74
+        headerByte(14) = &H0
+        headerByte(15) = &H80
+        headerByte(16) = &H1
+        headerByte(17) = &H50
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+    Private Sub GimbalButton3_Click(sender As Object, e As EventArgs) Handles GimbalButton3.Click
+        headerByte(9) = &H47
+        headerByte(10) = &H4A
+        headerByte(11) = &H19
+        headerByte(12) = &H0
+        headerByte(13) = &H80
+        headerByte(14) = &H22
+        headerByte(15) = &H99
+        headerByte(16) = &H39
+        headerByte(17) = &HDF
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+    Private Sub GimbalButton4_Click(sender As Object, e As EventArgs) Handles GimbalButton4.Click
+        headerByte(9) = &H47
+        headerByte(10) = &H4A
+        headerByte(11) = &H19
+        headerByte(12) = &H0
+        headerByte(13) = &H80
+        headerByte(14) = &HDE
+        headerByte(15) = &H66
+        headerByte(16) = &H79
+        headerByte(17) = &HDE
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+
+    Private Sub VideoOnButton_Click(sender As Object, e As EventArgs) Handles VideoOnButton.Click
+        headerByte(9) = &H7C
+        headerByte(10) = &H0
+        headerByte(11) = &H0
+        headerByte(12) = &H0
+        headerByte(13) = &H0
+        headerByte(14) = &H0
+        headerByte(15) = &H0
+        headerByte(16) = &H70
+        headerByte(17) = &H9F
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+    Private Sub VideoOffButton_Click(sender As Object, e As EventArgs) Handles VideoOffButton.Click
+        headerByte(9) = &H6F
+        headerByte(10) = &H0
+        headerByte(11) = &H0
+        headerByte(12) = &H0
+        headerByte(13) = &H0
+        headerByte(14) = &H0
+        headerByte(15) = &H0
+        headerByte(16) = &HB1
+        headerByte(17) = &HBD
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+    Private Sub FFCButton_Click(sender As Object, e As EventArgs) Handles FFCButton.Click
+        headerByte(9) = &H6D
+        headerByte(10) = &H1
+        headerByte(11) = &H3
+        headerByte(12) = &H0
+        headerByte(13) = &H0
+        headerByte(14) = &H0
+        headerByte(15) = &H0
+        headerByte(16) = &HA0
+        headerByte(17) = &HDB
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+
+    Private Sub ResetButton_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+        headerByte(9) = &H47
+        headerByte(10) = &HFE
+        headerByte(11) = &H0
+        headerByte(12) = &H0
+        headerByte(13) = &H0
+        headerByte(14) = &H0
+        headerByte(15) = &H0
+        headerByte(16) = &H6D
+        headerByte(17) = &H0
+
+        SerialPort1.Write(headerByte, 0, 18)
+    End Sub
+End Class

af3331d70157fd3061da952b4d2b2e437f85c684
davidane <54445028+davidane@users.noreply.github.com>
2023-03-31 13:11:47 -0600

    fish

--- IRForm.vb
+++ IRForm.vb
@@ -364,6 +364,10 @@ Public Class IRForm
         Me.Close()
     End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = "pressed"
     End Sub

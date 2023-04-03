d02d06805bbff0a3c25977124beb252fc9e92b5e
davidane <54445028+davidane@users.noreply.github.com>
2023-04-03 13:41:17 -0600

    added some buttons for changing the degree of turning, moved around some stuff on the front panel

--- IRForm.Designer.vb
+++ IRForm.Designer.vb
@@ -34,9 +34,9 @@ Partial Class IRForm
         Me.VideoOffButton = New System.Windows.Forms.Button()
         Me.FFCButton = New System.Windows.Forms.Button()
         Me.ResetButton = New System.Windows.Forms.Button()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.offModeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.directModeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.stabilizeModeRadioButton = New System.Windows.Forms.RadioButton()
         Me.upButton = New System.Windows.Forms.Button()
         Me.leftButton = New System.Windows.Forms.Button()
         Me.rightButton = New System.Windows.Forms.Button()
@@ -44,16 +44,19 @@ Partial Class IRForm
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-
         Me.Button2 = New System.Windows.Forms.Button()
-
         Me.daneButton = New System.Windows.Forms.Button()
-
+        Me.pointTwoDegreeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.oneDegreeRadioButton = New System.Windows.Forms.RadioButton()
+        Me.fiveDegreesRadioButton = New System.Windows.Forms.RadioButton()
+        Me.fifteenDegreesRadioButton = New System.Windows.Forms.RadioButton()
+        Me.twentyFiveDegreesRadioButton = New System.Windows.Forms.RadioButton()
+        Me.fourtyFiveDegreesRadioButton = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
         'GimbalButton1
         '
-        Me.GimbalButton1.Location = New System.Drawing.Point(316, 49)
+        Me.GimbalButton1.Location = New System.Drawing.Point(404, 7)
         Me.GimbalButton1.Name = "GimbalButton1"
         Me.GimbalButton1.Size = New System.Drawing.Size(75, 23)
         Me.GimbalButton1.TabIndex = 3
@@ -62,7 +65,7 @@ Partial Class IRForm
         '
         'GimbalButton2
         '
-        Me.GimbalButton2.Location = New System.Drawing.Point(317, 78)
+        Me.GimbalButton2.Location = New System.Drawing.Point(405, 36)
         Me.GimbalButton2.Name = "GimbalButton2"
         Me.GimbalButton2.Size = New System.Drawing.Size(75, 23)
         Me.GimbalButton2.TabIndex = 4
@@ -71,7 +74,7 @@ Partial Class IRForm
         '
         'GimbalButton3
         '
-        Me.GimbalButton3.Location = New System.Drawing.Point(317, 107)
+        Me.GimbalButton3.Location = New System.Drawing.Point(405, 65)
         Me.GimbalButton3.Name = "GimbalButton3"
         Me.GimbalButton3.Size = New System.Drawing.Size(75, 23)
         Me.GimbalButton3.TabIndex = 5
@@ -80,7 +83,7 @@ Partial Class IRForm
         '
         'GimbalButton4
         '
-        Me.GimbalButton4.Location = New System.Drawing.Point(317, 136)
+        Me.GimbalButton4.Location = New System.Drawing.Point(405, 94)
         Me.GimbalButton4.Name = "GimbalButton4"
         Me.GimbalButton4.Size = New System.Drawing.Size(75, 23)
         Me.GimbalButton4.TabIndex = 6
@@ -89,7 +92,7 @@ Partial Class IRForm
         '
         'HomeButton
         '
-        Me.HomeButton.Location = New System.Drawing.Point(317, 180)
+        Me.HomeButton.Location = New System.Drawing.Point(404, 123)
         Me.HomeButton.Name = "HomeButton"
         Me.HomeButton.Size = New System.Drawing.Size(75, 23)
         Me.HomeButton.TabIndex = 7
@@ -98,7 +101,7 @@ Partial Class IRForm
         '
         'VideoOnButton
         '
-        Me.VideoOnButton.Location = New System.Drawing.Point(235, 20)
+        Me.VideoOnButton.Location = New System.Drawing.Point(486, 7)
         Me.VideoOnButton.Name = "VideoOnButton"
         Me.VideoOnButton.Size = New System.Drawing.Size(75, 23)
         Me.VideoOnButton.TabIndex = 8
@@ -107,7 +110,7 @@ Partial Class IRForm
         '
         'VideoOffButton
         '
-        Me.VideoOffButton.Location = New System.Drawing.Point(235, 49)
+        Me.VideoOffButton.Location = New System.Drawing.Point(486, 36)
         Me.VideoOffButton.Name = "VideoOffButton"
         Me.VideoOffButton.Size = New System.Drawing.Size(75, 23)
         Me.VideoOffButton.TabIndex = 9
@@ -116,7 +119,7 @@ Partial Class IRForm
         '
         'FFCButton
         '
-        Me.FFCButton.Location = New System.Drawing.Point(141, 180)
+        Me.FFCButton.Location = New System.Drawing.Point(486, 65)
         Me.FFCButton.Name = "FFCButton"
         Me.FFCButton.Size = New System.Drawing.Size(75, 23)
         Me.FFCButton.TabIndex = 10
@@ -125,49 +128,49 @@ Partial Class IRForm
         '
         'ResetButton
         '
-        Me.ResetButton.Location = New System.Drawing.Point(222, 180)
+        Me.ResetButton.Location = New System.Drawing.Point(486, 94)
         Me.ResetButton.Name = "ResetButton"
         Me.ResetButton.Size = New System.Drawing.Size(75, 23)
         Me.ResetButton.TabIndex = 11
         Me.ResetButton.Text = "Reset Turret"
         Me.ResetButton.UseVisualStyleBackColor = True
         '
-        'RadioButton1
+        'offModeRadioButton
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(233, 94)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(39, 17)
-        Me.RadioButton1.TabIndex = 12
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "Off"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.offModeRadioButton.AutoSize = True
+        Me.offModeRadioButton.Location = New System.Drawing.Point(9, 19)
+        Me.offModeRadioButton.Name = "offModeRadioButton"
+        Me.offModeRadioButton.Size = New System.Drawing.Size(39, 17)
+        Me.offModeRadioButton.TabIndex = 12
+        Me.offModeRadioButton.TabStop = True
+        Me.offModeRadioButton.Text = "Off"
+        Me.offModeRadioButton.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'directModeRadioButton
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(233, 117)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(53, 17)
-        Me.RadioButton2.TabIndex = 13
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "Direct"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.directModeRadioButton.AutoSize = True
+        Me.directModeRadioButton.Location = New System.Drawing.Point(9, 42)
+        Me.directModeRadioButton.Name = "directModeRadioButton"
+        Me.directModeRadioButton.Size = New System.Drawing.Size(53, 17)
+        Me.directModeRadioButton.TabIndex = 13
+        Me.directModeRadioButton.TabStop = True
+        Me.directModeRadioButton.Text = "Direct"
+        Me.directModeRadioButton.UseVisualStyleBackColor = True
         '
-        'RadioButton3
+        'stabilizeModeRadioButton
         '
-        Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(233, 140)
-        Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(64, 17)
-        Me.RadioButton3.TabIndex = 14
-        Me.RadioButton3.TabStop = True
-        Me.RadioButton3.Text = "Stabilize"
-        Me.RadioButton3.UseVisualStyleBackColor = True
+        Me.stabilizeModeRadioButton.AutoSize = True
+        Me.stabilizeModeRadioButton.Location = New System.Drawing.Point(9, 65)
+        Me.stabilizeModeRadioButton.Name = "stabilizeModeRadioButton"
+        Me.stabilizeModeRadioButton.Size = New System.Drawing.Size(64, 17)
+        Me.stabilizeModeRadioButton.TabIndex = 14
+        Me.stabilizeModeRadioButton.TabStop = True
+        Me.stabilizeModeRadioButton.Text = "Stabilize"
+        Me.stabilizeModeRadioButton.UseVisualStyleBackColor = True
         '
         'upButton
         '
-        Me.upButton.Location = New System.Drawing.Point(87, 30)
+        Me.upButton.Location = New System.Drawing.Point(154, 12)
         Me.upButton.Name = "upButton"
         Me.upButton.Size = New System.Drawing.Size(75, 23)
         Me.upButton.TabIndex = 15
@@ -176,7 +179,7 @@ Partial Class IRForm
         '
         'leftButton
         '
-        Me.leftButton.Location = New System.Drawing.Point(26, 59)
+        Me.leftButton.Location = New System.Drawing.Point(93, 41)
         Me.leftButton.Name = "leftButton"
         Me.leftButton.Size = New System.Drawing.Size(75, 23)
         Me.leftButton.TabIndex = 16
@@ -185,7 +188,7 @@ Partial Class IRForm
         '
         'rightButton
         '
-        Me.rightButton.Location = New System.Drawing.Point(152, 59)
+        Me.rightButton.Location = New System.Drawing.Point(219, 41)
         Me.rightButton.Name = "rightButton"
         Me.rightButton.Size = New System.Drawing.Size(75, 23)
         Me.rightButton.TabIndex = 17
@@ -194,7 +197,7 @@ Partial Class IRForm
         '
         'downButton
         '
-        Me.downButton.Location = New System.Drawing.Point(87, 87)
+        Me.downButton.Location = New System.Drawing.Point(154, 69)
         Me.downButton.Name = "downButton"
         Me.downButton.Size = New System.Drawing.Size(75, 23)
         Me.downButton.TabIndex = 18
@@ -203,7 +206,7 @@ Partial Class IRForm
         '
         'ExitButton
         '
-        Me.ExitButton.Location = New System.Drawing.Point(317, 209)
+        Me.ExitButton.Location = New System.Drawing.Point(485, 123)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(75, 23)
         Me.ExitButton.TabIndex = 19
@@ -228,36 +231,103 @@ Partial Class IRForm
         Me.Label1.TabIndex = 21
         Me.Label1.Text = "Label1"
         '
-
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(222, 209)
+        Me.Button2.Location = New System.Drawing.Point(345, 212)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 22
         Me.Button2.Text = "Carson's"
         Me.Button2.UseVisualStyleBackColor = True
-
+        '
         'daneButton
         '
-        Me.daneButton.Location = New System.Drawing.Point(141, 208)
+        Me.daneButton.Location = New System.Drawing.Point(264, 211)
         Me.daneButton.Name = "daneButton"
         Me.daneButton.Size = New System.Drawing.Size(75, 23)
         Me.daneButton.TabIndex = 22
         Me.daneButton.Text = "Dane's Button"
         Me.daneButton.UseVisualStyleBackColor = True
-
+        '
+        'pointTwoDegreeRadioButton
+        '
+        Me.pointTwoDegreeRadioButton.AutoSize = True
+        Me.pointTwoDegreeRadioButton.Location = New System.Drawing.Point(320, 11)
+        Me.pointTwoDegreeRadioButton.Name = "pointTwoDegreeRadioButton"
+        Me.pointTwoDegreeRadioButton.Size = New System.Drawing.Size(75, 17)
+        Me.pointTwoDegreeRadioButton.TabIndex = 23
+        Me.pointTwoDegreeRadioButton.TabStop = True
+        Me.pointTwoDegreeRadioButton.Text = ".2 degrees"
+        Me.pointTwoDegreeRadioButton.UseVisualStyleBackColor = True
+        '
+        'oneDegreeRadioButton
+        '
+        Me.oneDegreeRadioButton.AutoSize = True
+        Me.oneDegreeRadioButton.Location = New System.Drawing.Point(320, 34)
+        Me.oneDegreeRadioButton.Name = "oneDegreeRadioButton"
+        Me.oneDegreeRadioButton.Size = New System.Drawing.Size(67, 17)
+        Me.oneDegreeRadioButton.TabIndex = 24
+        Me.oneDegreeRadioButton.TabStop = True
+        Me.oneDegreeRadioButton.Text = "1 degree"
+        Me.oneDegreeRadioButton.UseVisualStyleBackColor = True
+        '
+        'fiveDegreesRadioButton
+        '
+        Me.fiveDegreesRadioButton.AutoSize = True
+        Me.fiveDegreesRadioButton.Location = New System.Drawing.Point(320, 57)
+        Me.fiveDegreesRadioButton.Name = "fiveDegreesRadioButton"
+        Me.fiveDegreesRadioButton.Size = New System.Drawing.Size(72, 17)
+        Me.fiveDegreesRadioButton.TabIndex = 25
+        Me.fiveDegreesRadioButton.TabStop = True
+        Me.fiveDegreesRadioButton.Text = "5 degrees"
+        Me.fiveDegreesRadioButton.UseVisualStyleBackColor = True
+        '
+        'fifteenDegreesRadioButton
+        '
+        Me.fifteenDegreesRadioButton.AutoSize = True
+        Me.fifteenDegreesRadioButton.Location = New System.Drawing.Point(320, 80)
+        Me.fifteenDegreesRadioButton.Name = "fifteenDegreesRadioButton"
+        Me.fifteenDegreesRadioButton.Size = New System.Drawing.Size(78, 17)
+        Me.fifteenDegreesRadioButton.TabIndex = 26
+        Me.fifteenDegreesRadioButton.TabStop = True
+        Me.fifteenDegreesRadioButton.Text = "15 degrees"
+        Me.fifteenDegreesRadioButton.UseVisualStyleBackColor = True
+        '
+        'twentyFiveDegreesRadioButton
+        '
+        Me.twentyFiveDegreesRadioButton.AutoSize = True
+        Me.twentyFiveDegreesRadioButton.Location = New System.Drawing.Point(320, 103)
+        Me.twentyFiveDegreesRadioButton.Name = "twentyFiveDegreesRadioButton"
+        Me.twentyFiveDegreesRadioButton.Size = New System.Drawing.Size(78, 17)
+        Me.twentyFiveDegreesRadioButton.TabIndex = 27
+        Me.twentyFiveDegreesRadioButton.TabStop = True
+        Me.twentyFiveDegreesRadioButton.Text = "25 degrees"
+        Me.twentyFiveDegreesRadioButton.UseVisualStyleBackColor = True
+        '
+        'fourtyFiveDegreesRadioButton
+        '
+        Me.fourtyFiveDegreesRadioButton.AutoSize = True
+        Me.fourtyFiveDegreesRadioButton.Location = New System.Drawing.Point(320, 126)
+        Me.fourtyFiveDegreesRadioButton.Name = "fourtyFiveDegreesRadioButton"
+        Me.fourtyFiveDegreesRadioButton.Size = New System.Drawing.Size(78, 17)
+        Me.fourtyFiveDegreesRadioButton.TabIndex = 28
+        Me.fourtyFiveDegreesRadioButton.TabStop = True
+        Me.fourtyFiveDegreesRadioButton.Text = "45 degrees"
+        Me.fourtyFiveDegreesRadioButton.UseVisualStyleBackColor = True
         '
         'IRForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(400, 242)
-
+        Me.ClientSize = New System.Drawing.Size(831, 471)
+        Me.Controls.Add(Me.fourtyFiveDegreesRadioButton)
+        Me.Controls.Add(Me.twentyFiveDegreesRadioButton)
+        Me.Controls.Add(Me.fifteenDegreesRadioButton)
+        Me.Controls.Add(Me.fiveDegreesRadioButton)
+        Me.Controls.Add(Me.oneDegreeRadioButton)
+        Me.Controls.Add(Me.pointTwoDegreeRadioButton)
         Me.Controls.Add(Me.Button2)
-
         Me.Controls.Add(Me.daneButton)
-
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ExitButton)
@@ -265,9 +335,9 @@ Partial Class IRForm
         Me.Controls.Add(Me.rightButton)
         Me.Controls.Add(Me.leftButton)
         Me.Controls.Add(Me.upButton)
-        Me.Controls.Add(Me.RadioButton3)
-        Me.Controls.Add(Me.RadioButton2)
-        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.stabilizeModeRadioButton)
+        Me.Controls.Add(Me.directModeRadioButton)
+        Me.Controls.Add(Me.offModeRadioButton)
         Me.Controls.Add(Me.ResetButton)
         Me.Controls.Add(Me.FFCButton)
         Me.Controls.Add(Me.VideoOffButton)
@@ -294,9 +364,9 @@ Partial Class IRForm
     Friend WithEvents VideoOffButton As Button
     Friend WithEvents FFCButton As Button
     Friend WithEvents ResetButton As Button
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
-    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents offModeRadioButton As RadioButton
+    Friend WithEvents directModeRadioButton As RadioButton
+    Friend WithEvents stabilizeModeRadioButton As RadioButton
     Friend WithEvents upButton As Button
     Friend WithEvents leftButton As Button
     Friend WithEvents rightButton As Button
@@ -308,5 +378,10 @@ Partial Class IRForm
     Friend WithEvents Button2 As Button
 
     Friend WithEvents daneButton As Button
-
+    Friend WithEvents pointTwoDegreeRadioButton As RadioButton
+    Friend WithEvents oneDegreeRadioButton As RadioButton
+    Friend WithEvents fiveDegreesRadioButton As RadioButton
+    Friend WithEvents fifteenDegreesRadioButton As RadioButton
+    Friend WithEvents twentyFiveDegreesRadioButton As RadioButton
+    Friend WithEvents fourtyFiveDegreesRadioButton As RadioButton
 End Class
